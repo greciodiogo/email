443983f1e2281e11f8c60cc7bfdb3a1a417b838c
Grécio Diogo <fonebahia8@gmail.com>
2024-09-06 21:25:06 +0100

    fix: fix to json Text

--- src/main/java/com/ms/email/DTO/EmailRecordDTO.java
+++ src/main/java/com/ms/email/DTO/EmailRecordDTO.java
@@ -2,10 +2,13 @@ package com.ms.email.DTO;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record EmailRecordDTO(
     UUID userId,
     String emailTo,
     String subject,
+   @JsonProperty("text") // Mapeia o campo "text" do JSON para o campo "Text" no Java
     String Text
 ) {
     
